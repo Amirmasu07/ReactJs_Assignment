533839ff6e1988ccc200e3be1631165d58aba0d3
Amirmasu07 <115700409+Amirmasu07@users.noreply.github.com>
2023-05-25 21:27:38 +0530

    Add files via upload

---
+++ src/ReactCompo/01RouteQ.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+class RouteQ extends Component {
+
+    render() {
+        return (
+            <>
+            1888151515151515
+            </>
+        );
+    }
+}
+
+export default RouteQ;
